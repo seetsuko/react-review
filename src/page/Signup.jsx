e72371d8013be9e547cc5fef16bea4614069bef6
seetsuko <radgjms@gmail.com>
2023-02-24 02:21:16 +0900

    画像アップロード&インプットOK リサイズは途中

--- src/page/Signup.jsx
+++ src/page/Signup.jsx
@@ -1,3 +1,4 @@
+import "./Signup.css"
 import { useState } from "react";
 import axios from "axios";
 import { useForm } from "react-hook-form";
@@ -6,15 +7,36 @@ import { NavLink, useNavigate } from "react-router-dom";
 export const Signup = () =>{
 
   // ユーザー情報作成APIを使って、ユーザー作成画面を作成する
-  // ユーザアイコンも登録できるようにする
+  
   // 最近のスマホで撮影した写真などだと高画質でデータがとても大きいケースがあります。そのまま送ってしまうと重くなってしまう原因になってしまうのでサーバにアップロードする前にJavaScript側でリサイズしましょう。
   // いくつかライブラリはありますがCompressor.jsあたりがいいかと思います。
   
   const { register, handleSubmit, formState: {errors} } = useForm();
+  const [ iconUrl,setIconUrl ] = useState("")
   const [ cookie,setCookie ] = useState("")
-  const [ errorMessage,setErrorMessage ] =useState("")
+  const [ errorMessage,setErrorMessage ] = useState("")
   const navigate = useNavigate()
 
+  // 画像を表示できるようにする&Compressor.jsでリサイズ
+  const handleIconChange = (e) =>{
+    const iconFile = e.target.files[0];
+    const imageUrl = URL.createObjectURL(iconFile);
+    setIconUrl(imageUrl)
+
+    const img = new Compressor(data,{
+      qualty: 0.6,
+      success(result){
+        //圧縮が完了した時の処理を記述する
+      },
+      maxWidth:1000,
+      maxHeight: 400,
+      mimeType: 'image/png',
+      error(err) { 
+        //エラー処理 
+      },
+
+    }) 
+  }
 
   const onSubmit = (data) => {
     console.log(data);
@@ -31,12 +53,21 @@ export const Signup = () =>{
     })
     }
 
+    
+
   return (
     <div>
       <h2> 新規登録</h2>
       <p className='error-message'>{errorMessage}</p>
       <form onSubmit={ handleSubmit(onSubmit) } >
-      <div >
+      {/* ユーザアイコンも登録できるようにする */}
+        <div>
+          <div>
+            <img src={iconUrl} className="icon"/>
+          </div>
+          <input type="file" onChange={handleIconChange}/>
+        </div>
+        <div >
           <label htmlFor="signup-name">ユーザー名</label>
           <input
             id="signup-name" 
