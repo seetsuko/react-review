249819dcc89c718c9fcf29fcfa4d5ee5a75bf1a6
seetsuko <radgjms@gmail.com>
2023-03-05 23:02:34 +0900

    ページネーション完成

--- src/page/Paginate.jsx
+++
@@ -1,8 +0,0 @@
-export const Paginate = (props) =>{
-
-  return(
-    <div>
-      
-    </div>
-  )
-}
--- src/page/UserHome.jsx
+++ src/page/UserHome.jsx
@@ -3,54 +3,77 @@ import axios from "axios"
 import { useEffect, useState } from "react"
 import { url } from "../const";
 import { useCookies } from 'react-cookie';
-import { Paginate } from "./Paginate";
+import { Pagination } from "@mui/material";
 
 export const UserHome = () =>{
 
   const [ cookie ] = useCookies()
   const [ bookData,setBookData ] = useState([]);
   const [ queryNumber,setQueryNumber ] = useState(0)
+  const [ errorMessage,setErrorMessage ] = useState("")
 
-  console.log(cookie)
+  console.log(cookie.token)
 
+  // offsetのstateが変更するたびにAPIを叩く
   useEffect(()=>{  
-      axios 
-      .get(`${url}/books?offset=${queryNumber}`, {
-        headers: {
-        "Authorization": `Bearer ${cookie.token}`,
-      },
+    axios 
+    .get(`${url}/books?offset=${queryNumber}`, {
+      headers: {
+      "Authorization": `Bearer ${cookie.token}`,
+    },
+  })
+    .then((res)=>{
+      // レビューが無かったらメッセージを出す
+      if(res.data.length === 0){
+        setErrorMessage("レビューがありません")
+        setBookData([])
+      }else{
+      console.log(res.data)
+      setBookData(res.data)
+      // console.log(queryNumber)
+      setErrorMessage("")
+      }
     })
-      .then((res)=>{
-        console.log(res.data)
-        setBookData(res.data)
-      })
-      .catch(err => {
-        console.log("err:", err);
-      });
-  },[])
+    .catch(err => {
+      console.log("err:", err);
+    });
+  },[queryNumber])
 
+// ページが変わるとoffsetのstateが変わる
+const handlePaginate = (page) =>{
+  const pageNumber = page
+  setQueryNumber((pageNumber-1)*10)
+}
 
-  return(
-    <div>
-      <div className="title">
-        <h2 className="title__main">書籍一覧</h2>
-        <p className="title__sub">書籍レビューの一覧です</p>
-      </div>
-      <Paginate
-        queryNumber={queryNumber}
-      />
-      <div className="review">
-        <ul className="review-list">
-          {bookData.map((data) => {
-            return(
-            <li className="review-item" key={data.id}>
-              <p className="review-item__text">{data.title}</p>
-              <p className="review-item__text--detail">{data.review}</p>
-            </li>
-            )
-          })}
-        </ul>
-      </div>
+return(
+  <div className="main">
+    <div className="title">
+      <h2 className="title__main">書籍一覧</h2>
+      <p className="title__sub">書籍レビューの一覧です</p>
     </div>
-  )
+    <div className="review">
+      {/* ページネーションコンポーネント */}
+      <Pagination
+        count={10}          //総ページ数
+        color="secondary"     //ページネーションの色
+        //変更されたときに走る関数。第2引数にページ番号が入る
+        onChange={(e, page) =>{
+        handlePaginate(page)
+        }}  
+        className="paginate"
+      />
+    <p>{errorMessage}</p>
+      <ul className="review-list">
+        {bookData.map((data) => {
+          return(
+          <li className="review-item" key={data.id}>
+            <p className="review-item__text">{data.title}</p>
+            <p className="review-item__text--detail">{data.review}</p>
+          </li>
+          )
+        })}
+      </ul>
+    </div>
+  </div>
+)
 }
